7b1f7e57a4652bc2a31409fa5dc4fbef05a3319a
xsun12131 <971802674@qq.com>
2021-07-14 23:33:27 +0800

    update

--- src/components/common/Dialog.vue
+++ src/components/common/Dialog.vue
@@ -1,95 +1,113 @@
 <template>
-  <div class="dialog" v-show="showMask">
-    <div class="dialog-container">
-      <div class="dialog-title">{{ title }}</div>
-      <div class="content" v-html="content"></div>
-      <div class="btns">
-        <div v-if="type != 'reminder'" class="default-btn" @click="closeBtn">
-          {{ cancelText }}
+    <div class="dialog" v-show="showMask">
+        <div class="dialog-container">
+            <div class="dialog-title">{{ title }}</div>
+            <div class="content" v-html="content"></div>
+            <div class="btns">
+                <div v-if="type != 'reminder'" class="default-btn" @click="closeBtn">
+                    {{ cancelText }}
+                </div>
+                <div v-if="type == 'danger'" class="danger-btn" @click="dangerBtn">
+                    {{ dangerText }}
+                </div>
+                <div v-if="type == 'confirm'" class="confirm-btn" @click="confirmBtn">
+                    {{ confirmText }}
+                </div>
+            </div>
+            <div class="close-btn" @click="closeMask">
+                <i class="iconfont icon-close"></i>
+            </div>
         </div>
-        <div v-if="type == 'danger'" class="danger-btn" @click="dangerBtn">
-          {{ dangerText }}
-        </div>
-        <div v-if="type == 'confirm'" class="confirm-btn" @click="confirmBtn">
-          {{ confirmText }}
-        </div>
-      </div>
-      <div class="close-btn" @click="closeMask">
-        <i class="iconfont icon-close"></i>
-      </div>
     </div>
-  </div>
 </template>
 <script>
 export default {
-  name: "Dialog",
-  props: {
-    value: {},
-    // 类型包括 defalut 默认， danger 危险， confirm 确认， reminder 提示框（3秒自动消失）
-    type: {
-      type: String,
-      default: "default",
+    name: 'Dialog',
+    props: {
+        value: {},
+        // 类型包括 defalut 默认， danger 危险， confirm 确认， reminder 提示框（3秒自动消失）
+        type: {
+            type: String,
+            default: 'default',
+        },
+        content: {
+            type: String,
+            default: '',
+        },
+        title: {
+            type: String,
+            default: '',
+        },
+        cancelText: {
+            type: String,
+            default: '取消',
+        },
+        dangerText: {
+            type: String,
+            default: '删除',
+        },
+        confirmText: {
+            type: String,
+            default: '确认',
+        },
     },
-    content: {
-      type: String,
-      default: "",
+    data() {
+        return {
+            showMask: false,
+        };
     },
-    title: {
-      type: String,
-      default: "",
+    methods: {
+        closeMask() {
+            this.showMask = false;
+        },
+        closeBtn() {
+            this.$emit('cancel');
+            this.closeMask();
+        },
+        dangerBtn() {
+            this.$emit('danger');
+            this.closeMask();
+        },
+        confirmBtn() {
+            this.$emit('confirm');
+            this.closeMask();
+        },
     },
-    cancelText: {
-      type: String,
-      default: "取消",
+    mounted() {
+        this.showMask = this.value;
     },
-    dangerText: {
-      type: String,
-      default: "删除",
+    watch: {
+        value(newVal, oldVal) {
+            this.showMask = newVal;
+        },
+        showMask(val) {
+            this.$emit('input', val);
+            if (val == true && this.type === 'reminder') {
+                setTimeout(() => {
+                    this.closeMask();
+                }, 1000);
+            }
+        },
     },
-    confirmText: {
-      type: String,
-      default: "确认",
-    },
-  },
-  data() {
-    return {
-      showMask: false,
-    };
-  },
-  methods: {
-    closeMask() {
-      this.showMask = false;
-    },
-    closeBtn() {
-      this.$emit("cancel");
-      this.closeMask();
-    },
-    dangerBtn() {
-      this.$emit("danger");
-      this.closeMask();
-    },
-    confirmBtn() {
-      this.$emit("confirm");
-      this.closeMask();
-    },
-  },
-  mounted() {
-    this.showMask = this.value;
-  },
-  watch: {
-    value(newVal, oldVal) {
-      this.showMask = newVal;
-    },
-    showMask(val) {
-      this.$emit("input", val);
-      if (val == true && this.type === "reminder") {
-        setTimeout(() => {
-          this.closeMask();
-        }, 3000);
-      }
-    },
-  },
 };
 </script>
 <style scoped>
+.dialog {
+    position: absolute;
+    z-index: 2000;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: 600px;
+    background: #f2f6fc;
+}
+.dialog .dialog-title {
+    color: var(--theme);
+    padding: 5px;
+}
+
+.dialog .content {
+    line-height: 26px;
+    padding: 5px 20px;
+}
 </style>
--- src/components/note/NoteEditor.vue
+++ src/components/note/NoteEditor.vue
@@ -1,102 +1,136 @@
 <template>
-  <div class="note-editor">
-    <div class="title">
-      <label>标题</label>
-      <input v-model="title" />
+    <div class="note-editor">
+        <div class="title">
+            <label>标题</label>
+            <input
+                class="input w-100"
+                v-model="title"
+                maxlength="16"
+                autocomplete="off"
+                placeholder="请输入标题..."
+                name="title"
+                type="text"
+            />
+        </div>
+        <div class="opeartion">
+            <button class="save" @click="saveNote()">保存</button>
+        </div>
+        <div id="vditor" class="vditor"></div>
+        <Dialog v-model="sendVal" type="reminder" title="提示" content="保存成功"></Dialog>
     </div>
-    <div>
-      <button class="save" @click="saveNote()">保存</button>
-    </div>
-    <div id="vditor" class="vditor"></div>
-    <Dialog
-      v-model="sendVal"
-      type="reminder"
-      title="提示"
-      content="保存成功"
-    ></Dialog>
-  </div>
 </template>
 <script>
-import Vditor from "vditor";
-import "@/assets/css/vditor.css";
-import { save } from "@/api/note.js";
-import { fetchDataById } from "@/api/note";
-import Dialog from "@/components/common/Dialog.vue";
+import Vditor from 'vditor';
+import '@/assets/css/vditor.css';
+import { save } from '@/api/note.js';
+import { fetchDataById } from '@/api/note';
+import Dialog from '@/components/common/Dialog.vue';
 
 export default {
-  name: "NoteEditor",
-  components: {
-    Dialog,
-  },
-  data() {
-    return {
-      id: "",
-      title: "",
-      contentEditor: "",
-      sendVal: false,
-    };
-  },
-  methods: {
-    saveNote() {
-      const note = {};
-      note.id = this.id;
-      note.title = this.title;
-      note.content = this.contentEditor.getValue();
-      save(note).then((data) => {
-        this.id = data.id;
-        this.sendVal = true;
-      });
+    name: 'NoteEditor',
+    components: {
+        Dialog,
     },
-    getNoteDetail: function (id) {
-      fetchDataById(id).then((note) => {
-        this.id = note.id;
-        this.title = note.title;
-        this.contentEditor.setValue(note.content);
-      });
+    data() {
+        return {
+            id: '',
+            title: '',
+            contentEditor: '',
+            sendVal: false,
+        };
     },
-  },
-  mounted() {
-    this.contentEditor = new Vditor("vditor", {
-      toolbarConfig: {
-        pin: true,
-      },
-      cache: {
-        enable: false,
-      },
-      after: () => {
-        if (this.$route.query && this.$route.query.id) {
-          // vue从路径中获取参数 此处参数名为id
-          const id = this.$route.query.id;
-          this.getNoteDetail(id);
-        }
-      },
-    });
-  },
+    methods: {
+        saveNote() {
+            const note = {};
+            note.id = this.id;
+            note.title = this.title;
+            note.content = this.contentEditor.getValue();
+            save(note).then(data => {
+                this.id = data.id;
+                this.sendVal = true;
+            });
+        },
+        getNoteDetail: function(id) {
+            fetchDataById(id).then(note => {
+                this.id = note.id;
+                this.title = note.title;
+                this.contentEditor.setValue(note.content);
+            });
+        },
+    },
+    mounted() {
+        this.contentEditor = new Vditor('vditor', {
+            toolbarConfig: {
+                pin: true,
+            },
+            cache: {
+                enable: false,
+            },
+            after: () => {
+                if (this.$route.query && this.$route.query.id) {
+                    // vue从路径中获取参数 此处参数名为id
+                    const id = this.$route.query.id;
+                    this.getNoteDetail(id);
+                }
+            },
+        });
+    },
 };
 </script>
 <style scoped>
-@import "../../assets/css/markdown.css";
+@import '../../assets/css/markdown.css';
 
 .note-editor {
-  width: 100%;
-  background-color: rgb(235, 243, 243);
+    width: 100%;
+    background-color: rgb(235, 243, 243);
 }
 
+.title {
+    display: flex;
+    align-content: center;
+    align-items: center;
+    padding: 15px;
+}
 .title label {
-  margin-bottom: 10px;
-  -webkit-line-clamp: 2;
-  -webkit-box-orient: vertical;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  word-break: break-word;
-  color: var(--main);
-  font-size: 18px;
-  line-height: 24px;
-  max-height: 48px;
-  transition: color 0.35s;
+    width: 60px;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    word-break: break-word;
+    color: var(--main);
+    font-size: 18px;
+    line-height: 24px;
+    max-height: 48px;
+    transition: color 0.35s;
+}
+.title .input {
+    background: var(--classC);
+    height: 34px;
+    border: 1px solid transparent;
+    padding: 0 18px;
+    color: var(--routine);
+    transition: background 0.35s, border-color 0.35s, padding-right 0.35s;
+    border-radius: 17px 0 0 17px;
+}
+.opeartion {
+    height: 32px;
+    padding: 0 15px;
+    margin: 5px 0;
+}
+.save {
+    float: right;
+    right: 15px;
+    border-radius: 3px;
+    height: 32px;
+    padding: 0 15px;
+    border: none;
+    background: var(--theme);
+    color: #fff;
+    font-size: 14px;
 }
 
 .vditor {
-  min-height: calc(100% - 45px);
+    min-height: calc(100% - 45px);
 }
 </style>
